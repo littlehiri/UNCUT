844c7bdcdf196d653b8f94ed4415afe016bf17e7
Álvaro Hidalgo Riquelme <alvarohiri@gmail.com>
2023-04-18 13:55:59 +0200

    death

---
+++ Assets/Scripts/Enemy/EnemyDeath.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDeath : MonoBehaviour
+{
+    public void EnemyDeathController()
+    {
+        //Desactivamos al enemigo padre
+        transform.gameObject.SetActive(false);
+        //Instanciamos el efecto de muerte del enemigo en la posición del primer hijo
+        //Instantiate(deathEffect, transform.GetChild(0).position, transform.GetChild(0).rotation);
+    }
+}
